5ed969685cf2f2eddcc4b72cd62a6bfd78b21fc2
jangrubin <wkdfnqls2465@gmail.com>
2023-04-29 11:40:21 +0900

    html 파일 sync

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import http from "http";
-
+import fs from "fs";
 const server =http.createServer((req,res)=> {
   res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(fs.readFileSync("./index.html", "utf-8"));
   res.end();
 });
 server.listen(8080, (err)=> {
