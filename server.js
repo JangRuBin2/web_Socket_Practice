53072606e32099c82bcfba24259ec8dcfe8b05b6
jangrubin <wkdfnqls2465@gmail.com>
2023-04-29 11:46:24 +0900

    http서버 + io서버 connect

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import http from "http";
 import fs from "fs";
+import io from "socket.io";
+
 const server =http.createServer((req,res)=> {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write(fs.readFileSync("./index.html", "utf-8"));
   res.end();
 });
+
+// const io = new Server(server);
+// console.log(io)
+// 소켓 서버를 http에 연결
+io.listen(server);
 server.listen(8080, (err)=> {
   if(err) {
     console.log(err);
