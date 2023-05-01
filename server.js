ecf6c8c44357267977efbbacb0419e82754df7ef
jangrubin <wkdfnqls2465@gmail.com>
2023-05-01 16:43:46 +0900

    중복코드 제거 및 웹소켓 서버 생성

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 import http from "http";
+import fs from "fs";
+import {Server} from "socket.io";
 
 const server =http.createServer((req,res)=> {
   res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(fs.readFileSync("./index.html", "utf-8"));
   res.end();
+});
+
+// const io = new Server(server);
+// console.log(io)
+// 소켓 서버를 http에 연결
+const io = new Server(server);
+io.on("connection", (socket)=> {
+  console.log("유저가 접속했다")
+  socket.on("discconnect",()=> {
+    console.log("유저가 탈주했다.");
+  });
 });
 server.listen(8080, (err)=> {
   if(err) {
