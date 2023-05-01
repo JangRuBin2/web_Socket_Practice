a705630985b3e69c6758025beaca64dae078c6e7
jangrubin <wkdfnqls2465@gmail.com>
2023-05-01 17:50:30 +0900

    클라이언트가 전송한 데이터가 서버로 오질 않음

--- server.js
+++ server.js
@@ -12,13 +12,17 @@ const server =http.createServer((req,res)=> {
 // console.log(io)
 // 소켓 서버를 http에 연결
 const io = new Server(server);
+// !연결 되면
 io.on("connection", (socket)=> {
-  console.log("유저가 접속했다")
+  console.log("유저가 접속했다");
+  //! 메시지 받으면
+  socket.on("send Message",(data)=> {
+    console.log(data);
+    io.emit("send Message", data);
+  });
+  //! 연결 끊기면
   socket.on("discconnect",()=> {
     console.log("유저가 탈주했다.");
-  });
-  socket.on("send Message",(chatLog)=> {
-    console.log(chatLog);
   });
 });
 server.listen(8080, (err)=> {
